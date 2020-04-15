a5c4e01cd246813a8bbaea72fe8c0b4d36d70607
李祖明 <solace.lee@outlook.com>
2020-04-15 23:00:37 +0800

    1

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 export default {
-    // db: 'mongodb://127.0.0.1:27017/code-house',
+    db: 'mongodb://127.0.0.1:27017/code-house',
     // db: 'mongodb://solace:solace6637296@www.cleanown.cn:27017/code-house',
-    db: 'mongodb://www.cleanown.cn:27017/code-house',
+    // db: 'mongodb://www.cleanown.cn:27017/code-house',
     port: 3000,
     base: 'http://localhost:3000',
     qiniu: {
