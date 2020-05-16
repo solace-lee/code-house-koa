a1b709543f40c7e40f5c89ca7ed247712d5de90c
李祖明 <solace.lee@outlook.com>
2020-05-16 21:58:42 +0800

    https

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 export default {
     db: 'mongodb://127.0.0.1:27017/code-house',
-    prefix: 'http://api.cleanown.cn/upload/images/',
+    prefix: 'https://api.cleanown.cn/upload/images/',
     port: 3000,
     base: 'http://localhost:3000',
   }
--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 export default {
     db: 'mongodb://127.0.0.1:27017/code-house',
-    prefix: 'http://api.cleanown.cn/upload/images/',
+    prefix: 'https://api.cleanown.cn/upload/images/',
     port: 3000,
     base: '47.107.105.20',
   }
